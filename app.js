85b311e7d7a50a2b30f5c285cef172aff7701e3a
nicholasscabral <nicholasscabral@gmail.com>
2020-11-15 21:48:24 -0300

    update data route

--- app.js
+++ app.js
@@ -64,21 +64,58 @@ app.post('/game', (req, res) => {
   })
 })
 
-app.delete('/game/:id', (req, res) => {
+app.post('/delete-game/:id', (req, res) => {
 
   const id = req.params.id
 
-  if (isNaN(id)) res.sendStatus(400)
+  if (isNaN(id)) return res.status(400)
   else {
-    db.query("SELECT * FROM games WHERE id = ?", id, (err, result) => {
-      if (err) res.sendStatus(500)
-      else if (result.length === 0) return res.status(404).send("GAME NOT FOUND")
-        else {
-          db.query("DELETE FROM games WHERE id = ?", id, (err, result) => {
-            if (err) res.sendStatus(500)
-            else res.status(200).send("GAME DELETED ")
-          })
-        }
+    db.query('DELETE FROM games WHERE id = ?', id, (err, result) => {
+      if (err) return res.status(404).send("GAME NOT FOUND")
+      else return res.status(200).send("GAME DELETED")
+    })
+  }
+})
+
+app.post('/update-game/:id', (req, res) => {
+
+  const id = req.params.id
+  const { title, price, year } = req.body
+  const defaultQuery = 'UPDATE games SET '
+  let query = defaultQuery
+  const queryData = []
+
+  if (title) {
+    query += 'title=?, '
+    queryData.push(title)
+  }
+  
+  if (price) {
+    query += 'price=?, '
+    queryData.push(price)
+  }
+
+  if (year) {
+    query += 'year=?, '
+    queryData.push(year)
+  }
+
+  if (query === defaultQuery) res.sendStatus(500)
+  else {
+    query = query.trim()
+    query = query.slice(0, query.length - 1)
+    query = query + ' WHERE id=?'
+    queryData.push(id)
+
+    db.query(query, queryData, (err, result) => {
+      if (err) {
+        console.log(err)
+        res.sendStatus(400)
+      }
+      else {
+        console.log(result)
+        return res.status(200).send("DADOS ATUALIZADOS")
+      }
     })
   }
 })
